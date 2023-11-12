093f2910254f04d82e13c020f3fc243dafe670e3
Aliakbar Soltani <aliakbarsoltaniiiii@gmail.com>
2023-11-12 20:56:55 +0330

    1. It shows the number of spaces

--- Program.cs
+++ Program.cs
@@ -11,6 +11,27 @@ namespace Pascal_Case_Console
         static void Main(string[] args)
         {
 
+            string Text = Console.ReadLine();
+            bool fHasSpace = Text.Contains(" ");
+            if (fHasSpace == true)
+            {
+                int Count_Space = 0;
+                foreach (char item in Text)
+                {
+                    if (item == ' ')
+                    {  
+                        Count_Space++; 
+                    }
+                }
+                
+                Console.WriteLine("Spaces : {0}",Count_Space);
+            }
+            else
+            {
+                Console.WriteLine("This Can't Be Converted");
+            }
+
+            Console.ReadKey();
         }
     }
 }
