61a10977ee627fddd95531f4b203c9f06fd2a98a
Aliakbar Soltani <aliakbarsoltaniiiii@gmail.com>
2023-11-14 19:57:15 +0330

    complete the program for Step1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,29 +9,31 @@ namespace Pascal_Case_Console
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            int Count_Space = 0;
+            int Count_Character = 0;
 
             string Text = Console.ReadLine();
             bool fHasSpace = Text.Contains(" ");
-            if (fHasSpace == true)
-            {
-                int Count_Space = 0;
                 foreach (char item in Text)
                 {
                     if (item == ' ')
-                    {  
-                        Count_Space++; 
+                    {
+                        Count_Space++;
                     }
                 }
-                
-                Console.WriteLine("Spaces : {0}",Count_Space);
-            }
-            else
-            {
-                Console.WriteLine("This Can't Be Converted");
-            }
+                foreach (char item in Text)
+                {
+                    Count_Character++;
+                }
 
+                TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+                Console.WriteLine(myTI.ToTitleCase(Text));
+                Console.WriteLine("Spaces : {0}", Count_Space);
+                Console.WriteLine("Character : {0}", Count_Character);
+            
             Console.ReadKey();
         }
     }
